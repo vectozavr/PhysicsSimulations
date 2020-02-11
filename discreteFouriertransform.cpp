95f67481ae2fbb725a40595696a4e081c22b7df5
Vectozavr <ivanilin420@gmail.com>
2020-02-12 00:55:41 +0700

    Fourier transform 1 version

---
+++ discreteFouriertransform.cpp
@@ -0,0 +1,77 @@
+//
+// Created by ivan- on 11.02.2020.
+//
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include "settings.h"
+#include "gnuplot.h"
+
+struct Complex {
+    double real = 0;
+    double imagine = 0;
+};
+
+struct Point2D {
+    double x = 0;
+    double y = 0;
+
+    Point2D& operator+=(const Point2D& point2D) { this->x += point2D.x; this->y += point2D.y; }
+    Point2D& operator=(const Point2D& point2D) { this->x = point2D.x; this->y = point2D.y; return *this; }
+    Point2D& operator*(double number) { this->x *= number; this->y *= number; }
+    Point2D operator-(const Point2D& point2D) const { return {this->x - point2D.x, this->y - point2D.y}; }
+    Point2D operator+(const Point2D& point2D) const { return {this->x + point2D.x, this->y + point2D.y}; }
+
+    Point2D normalize() { return Point2D{this->x/abs(), this->y/abs()};}
+    double abs() {return sqrt(x*x + y*y); }
+};
+
+bool saveVectorPoint2DToFile(const std::vector<Point2D>& data, const std::string& fileName = "data.dat") {
+    std::ofstream _ofstream(fileName);
+    if(!_ofstream.is_open())
+        return false;
+
+    for(auto d : data)
+        _ofstream << d.x << "   " << d.y << std::endl;
+
+    _ofstream.close();
+    return true;
+}
+
+void fourierTransform(const std::vector<Point2D>& data, std::vector<Point2D>& transform) {
+    transform.clear();
+    for(int k = 0; k < data.size(); k++) {
+        Point2D transformed = {0, 0};
+        for(int n = 0; n < data.size(); n++) {
+            transformed.x += data[n].y*cos(2*PI*k*n/data.size());
+            transformed.y += -data[n].y*sin(2*PI*k*n/data.size());
+        }
+        transform.push_back({data[k].x, transformed.abs()/data.size()});
+    }
+}
+
+int main() {
+    std::vector<Point2D> data;
+    std::vector<Point2D> transform;
+
+    int quantity = 1000;
+    for(int i = 0; i < quantity; i++) {
+        data.push_back({2*PI*(double)i/quantity, sin(2*PI*(double)i/quantity) + sin(8*PI*(double)i/quantity)});
+    }
+
+    fourierTransform(data, transform);
+
+
+
+    saveVectorPoint2DToFile(data, "data.dat");
+    saveVectorPoint2DToFile(transform, "transform.dat");
+
+    GnuplotPipe gp;
+    gp.sendLine(R"(set xrange [-5:10])");
+    gp.sendLine(R"(set yrange [-2:2])");
+    gp.sendLine(R"(plot "data.dat" with lines, "transform.dat" with lines)");
+
+    return 0;
+
+}
--- turtle.cpp
+++ turtle.cpp
@@ -52,7 +52,7 @@ int main() {
     auto tp1 = chrono::system_clock::now();
     auto tp2 = chrono::system_clock::now();
 
-    unsigned long long N = 10;
+    unsigned long long N = 25;
     double R = 30*100;
     double velocity = 30;
 
